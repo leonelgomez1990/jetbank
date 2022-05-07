1915106a95cb5aab04d03a7bac511fbef1681236
leonelgomez1990 <leonelgomez@live.com.ar>
2022-05-06 23:43:36 -0300

    password visibility and toggle button

--- app/src/main/java/com/lgomez/jetbank/MainActivity.kt
+++ app/src/main/java/com/lgomez/jetbank/MainActivity.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lgomez.jetbank.ui.theme.JetbankTheme
@@ -74,10 +78,23 @@ private fun PasswordField(
     value: String,
     onValueChange: (String) -> Unit,
 ) {
+    var passVisible by remember { mutableStateOf(false) }
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(text = "Password") }
+        label = { Text(text = "Password") },
+        visualTransformation = if (passVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            IconToggleButton(
+                checked = passVisible,
+                onCheckedChange = { passVisible = it }
+            ) {
+                val icon =
+                    if (passVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility
+                Icon(imageVector = icon, contentDescription = null)
+
+            }
+        }
     )
 }
 
