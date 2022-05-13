f7bbf9619b86b35cb56df7a1271e0dfe788634a1
leonelgomez1990 <leonelgomez@live.com.ar>
2022-05-12 21:52:19 -0300

    trying Column, Row and state

--- app/src/main/java/com/lgomez/jetbank/ui/screens/Main.kt
+++ app/src/main/java/com/lgomez/jetbank/ui/screens/Main.kt
@@ -1,11 +1,19 @@
 package com.lgomez.jetbank.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lgomez.jetbank.ui.configuration.Screen
+import com.lgomez.jetbank.ui.theme.JetbankTheme
 
 @Composable
 fun Main() {
@@ -17,6 +25,45 @@ fun Main() {
                 text = "Main Screen",
                 style = MaterialTheme.typography.h5
             )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    JetbankTheme {
+        MyApp()
+    }
+}
+
+
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false)}
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
     }
 }
