861d50457995ba38ffd5faab40eef035ad6032f3
leonelgomez1990 <leonelgomez@live.com.ar>
2022-06-20 17:52:39 -0300

    added viewmodel SignIn and splash

--- app/src/main/java/com/lgomez/jetbank/login/ui/viewmodels/SignInViewModel.kt
+++ app/src/main/java/com/lgomez/jetbank/login/ui/viewmodels/SignInViewModel.kt
@@ -1,13 +1,15 @@
 package com.lgomez.jetbank.login.ui.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.lgomez.jetbank.core.utils.MyResult
+import com.lgomez.jetbank.login.ui.domain.AuthCredentials
+import com.lgomez.jetbank.login.ui.navigatorstates.SignInNavigatorStates
 import com.lgomez.jetbank.login.usecases.SignInWithEmailAndPasswordUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,23 +21,53 @@ class SignInViewModel @Inject constructor(
         const val TAG = "SignInViewModel"
     }
 
-    private val _isSuccessfulLoggued: MutableLiveData<Boolean> = MutableLiveData()
-    val isSuccessfulLoggued: LiveData<Boolean> get() = _isSuccessfulLoggued
+    private val _navigation = MutableStateFlow<SignInNavigatorStates>(SignInNavigatorStates.Here)
+    val navigation: StateFlow<SignInNavigatorStates> = _navigation.asStateFlow()
+
+    private val loginCredentialsData = MutableLiveData<AuthCredentials>()
+
+    private val _userName = MutableLiveData("")
+    val userName: LiveData<String> = _userName
+
+    private val _userPassword = MutableLiveData("")
+    val userPassword: LiveData<String> = _userPassword
 
     init {
-        _isSuccessfulLoggued.value = false
     }
 
-    fun signInUser(email: String, password: String) {
-        viewModelScope.launch {
-            when (val result = signInWithEmailAndPasswordUseCase(email, password)) {
-                is MyResult.Failure -> {}
-                is MyResult.Success -> { _isSuccessfulLoggued.value = true }
-                is MyResult.Loading -> {}
+    fun goToSignUp() {
+        _navigation.value = SignInNavigatorStates.ToSignUp
+    }
+
+    fun goToMenuFeature() {
+        _navigation.value = SignInNavigatorStates.ToMenuFeature
+    }
+
+    fun setLoginCredentials(loginCredentials: AuthCredentials) {
+        loginCredentialsData.value = loginCredentials
+    }
+
+    val getLoginResult = loginCredentialsData.switchMap { authCredentials ->
+        liveData(Dispatchers.IO) {
+            emit(MyResult.Loading())
+            try {
+                emit(
+                    signInWithEmailAndPasswordUseCase(
+                        authCredentials.username,
+                        authCredentials.password
+                    )
+                )
+            } catch (e: Exception) {
+                emit(MyResult.Failure(e))
             }
         }
-
     }
 
-    fun isSuccessfulLoggued() = _isSuccessfulLoggued.value ?: false
+    fun onUserNameChange(newValue: String) {
+        _userName.value = newValue
+    }
+
+    fun onUserPasswordChange(newValue: String) {
+        _userPassword.value = newValue
+    }
 }
---
+++ app/src/main/java/com/lgomez/jetbank/login/ui/viewmodels/SplashViewModel.kt
@@ -0,0 +1,25 @@
+package com.lgomez.jetbank.login.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.lgomez.jetbank.login.ui.navigatorstates.SplashNavigatorStates
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+) : ViewModel() {
+
+    companion object {
+        const val TAG = "SplashViewModel"
+    }
+
+    private val _navigation = MutableStateFlow<SplashNavigatorStates>(SplashNavigatorStates.Here)
+    val navigation: StateFlow<SplashNavigatorStates> = _navigation.asStateFlow()
+
+    fun goToSignIn() {
+        _navigation.value = SplashNavigatorStates.ToSignIn
+    }
+}
